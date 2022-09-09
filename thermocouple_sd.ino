fef9785495badbd4face9375018eb75aed3d6380
josephckaram <103529553+josephckaram@users.noreply.github.com>
2022-09-09 12:07:38 -0700

    Add files via upload

---
+++ thermocouple_sd.ino
@@ -0,0 +1,100 @@
+//libraries/files included
+#include <MAX6675.h>
+#include <SD.h>
+#include <SPI.h>
+
+//pin assignments
+#define CS_PIN1 13
+#define CS_PIN2 12
+#define soPin = 50;
+#define sckPin = 52;
+
+//thermocouple initialization
+MAX6675 tcouple1(CS_PIN1);
+MAX6675 tcouple2(CS_PIN2);
+
+File myFile; //file object
+
+//data variables
+int count = 0;
+double temperature1 = 0;
+double temperature2 = 0;
+char myFileName[] = "test1.txt";
+  
+void setup() {
+  //initialize serial
+  Serial.begin(9600);
+
+   //SD card initialization
+  Serial.println("Initializing SD card...");
+  if (!SD.begin(53)) {
+    Serial.println("Initialization failed!");
+    while(1);
+  }
+  Serial.println("Initialization done.");
+  Serial.println();
+  
+ // if (SD.exists("data.txt")) SD.remove("data.txt"); //if there's already a file, delete it
+
+//check the files name and create a new file
+  for (int i = 1; i < 1000; i++) {
+  char y = i+'0';
+  myFileName[4] = y; // change the file name: test1.txt test2.txt.......
+  
+  if (SD.exists(myFileName)){ // if the file already exist
+    continue;
+    }
+ 
+    Serial.println(myFileName); // Print the filename to which data is written before each run. 
+    break;
+  }
+  
+  //timer/counter1 initialization
+  cli(); //stop interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+  OCR1A = 15624; // 1 Hz
+  TCCR1B |= (1 << WGM12); //ctc mode
+  TCCR1B |= (1 << CS12) | (1 << CS10); //prescaler
+  TIMSK1 |= (1 << OCIE1A);
+  sei(); //start interrupts
+}
+
+ISR(TIMER1_COMPA_vect) { //interrupt function to be called when timer reaches max
+  count++; //increment count each second 
+}
+
+void loop() {
+
+//run the part every 5 second.
+if (count == 5) {
+  
+        //get thermocouples reading
+        temperature1 = tcouple1.readTempC();
+        temperature2 = tcouple2.readTempC();
+        
+        //print data to serial monitor
+        Serial.print("Thermocouple 1 Temperature: ");
+        Serial.println(temperature1);
+        Serial.print("Thermocouple 2 Temperature: ");
+        Serial.println(temperature2);
+        Serial.println();
+        
+        //print data to sd card file
+        myFile = SD.open(myFileName, FILE_WRITE);
+        myFile.seek(EOF);
+
+        myFile.print("Thermocouple 1 Temperature: ");
+        myFile.println(temperature1);
+        myFile.print("Thermocouple 2 Temperature: ");
+        myFile.println(temperature2);
+
+        myFile.println();
+        
+        myFile.close();
+
+        count = 0; //reset count
+      }
+  if (count > 5) count = 0; //if count somehow exceeded max, reset it
+}
